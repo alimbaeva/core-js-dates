37618336153eafcb16c8f4aaa70543c8427bf924
Asel <asel.alimbaeva.kk@gmail.com>
2024-03-21 06:18:21 +0300

    feat: resolved getCountDaysOnPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -106,8 +106,14 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const startDate = new Date(dateStart);
+  const endDate = new Date(dateEnd);
+
+  const difference = endDate - startDate;
+  const daysDifference = Math.ceil(difference / (1000 * 60 * 60 * 24));
+
+  return daysDifference + 1;
 }
 
 /**
